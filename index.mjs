b51f3fd279a6eb370b4201d770b93c7baae23dd8
Tamiriscoliveira <tami_coliveira@hotmail.com>
2025-11-28 14:43:59 -0300

    teste

---
+++ index.mjs
@@ -0,0 +1,13 @@
+import { log } from './log.mjs';
+
+
+export const handler = async (event) => {
+
+    log('log de execução event: ' + JSON.stringify(event));
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(event),
+    };
+
+};
